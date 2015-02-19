79a34b23a3302e3546706c60d67e281a895d4a8d
oscarrel <oscarrela@yahoo.es>
2015-02-19 14:26:27 +0100

    Panel info

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    if ($(window).width() <= 620) {
+        //alert('Su resolución de pantalla es igual o menor a 520px');
+        //$('#btn_alum').html('lalala');
+        console.log("hole hola");
+    }
+});
